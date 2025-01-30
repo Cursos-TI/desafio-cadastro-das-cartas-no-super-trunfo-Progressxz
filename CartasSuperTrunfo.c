c4079256aaf3379f64bd855c0fbd2e2ec0157de9
Progressxz <njoaocarlos48@gmail.com>
2025-01-30 15:14:30 +0000

    codigo quase finalizado

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,7 +4,7 @@ int main() {
     //Declaração das variáveis
     char codigo[4]; //Codigo da cidade "A01"
     char nome[50];
-    int população;
+    int populacao;
     float area;
     double pib;
     int pontos_turisticos;
@@ -12,29 +12,36 @@ int main() {
     //introdução e cadastro das cartas
     printf("Desafio Super Trunfo - Paises\n");
     printf("Cadastro de Carta - Super Trunfo Paises\n");
+
     printf("Digite o codigo da cidade: (ex: A01): ");
     scanf("%3s", codigo);
+
     printf("Digite o nome da cidade: ");
     scanf (" %[^\n]", nome);  //lê uma string com espaços
+    getchar();
 
-    printf("Digite a população da cidade");
-    scanf ("%d", &população);
+    printf("Digite a população da cidade: ");
+    scanf ("%d", &populacao);
+    while (getchar() != '\n');
 
-    printf("Digite a área da cidade (km²)");
+    printf("Digite a área da cidade: (km²) ");
     scanf("%f", &area); 
+    while (getchar() != '\n');
 
-    prinf("Digite o PIB da cidade (em bilhões): ");
+    printf("Digite o PIB da cidade: (em bilhões): ");
     scanf("%lf", &pib);  //lê o pib em bilhões
+    while (getchar() != '\n');
 
     printf("Digite o número de pontos turísticos: ");
     scanf("%d",  &pontos_turisticos);
+    while (getchar() != '\n');
 
 
     //dados
     printf("\nDados da carta cadastrada:\n");
     printf("Código: %s\n", codigo);
     printf("Nome: %s\n", nome);
-    printf("População: %d, habitantes\n", população);
+    printf("População: %d, habitantes\n", populacao);
     printf("Área: %.2f km²\n", area);
     printf("PIB: %.21f bilhões\n", pib);
     printf("Pontos Turísticos: %d\n", pontos_turisticos);
